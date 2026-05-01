8516b279fda72cdf3540f79b57f518b5b4a42a2a
NikLa <nikita@euro-net.com.ua>
2026-05-02 00:25:32 +0300

    check env

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -4,3 +4,5 @@ export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
 )
+console.log("ENV URL:", import.meta.env.VITE_SUPABASE_URL)
+console.log("ENV KEY:", import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY)
